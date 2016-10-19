a9c6e11d1b44a57ad310f2166e6a825633d66648
Riturajcse <riturajcse1994@gmail.com>
2016-10-19 19:03:58 +0530

    changing calllll11133

--- calculator1.php
+++ calculator1.php
@@ -14,7 +14,7 @@ class Calculator{
   }
 
   public function multiply($numbers_to_multiply){
-      $product = 1666;
+      $product = 16616;
       foreach($numbers_to_multiply as $num){
           $product = $num * $product;
       }
