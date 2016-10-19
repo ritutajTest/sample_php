38a16e81b731cdb8d1130184355f19f127ebc7ef
Riturajcse <riturajcse1994@gmail.com>
2016-10-19 18:49:11 +0530

    changing cl1111

--- calculator1.php
+++ calculator1.php
@@ -14,7 +14,7 @@ class Calculator{
   }
 
   public function multiply($numbers_to_multiply){
-      $product = 1111;
+      $product = 1111111666;
       foreach($numbers_to_multiply as $num){
           $product = $num * $product;
       }
