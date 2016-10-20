983d1d8a084968e8252237a61c76e5cd19b61625
Riturajcse <riturajcse1994@gmail.com>
2016-10-20 10:20:47 +0530

    updates clc3333

--- calculator.php
+++ calculator.php
@@ -14,7 +14,7 @@ class Calculator{
   }
 
   public function multiply($numbers_to_multiply){
-      $product = 411;
+      $product = 4113;
       foreach($numbers_to_multiply as $num){
           $product = $num / $product;
       }
